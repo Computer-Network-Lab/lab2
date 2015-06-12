401795cf7b3afa5d52070bef7ad89171914acfa9
Dangyi Liu <LeedyPKU@gmail.com>
2015-06-12 16:21:53 +0800

    add duplication detection

--- crawler/github.rb
+++ crawler/github.rb
@@ -4,6 +4,7 @@ require "curb"
 Dir.chdir(__dir__)
 
 %w[ C C++ CSS JavaScript PHP Python Ruby ].each do |lang|
+  next if File.exists? lang
   html = Nokogiri.parse Curl.get("https://github.com/trending?l=#{lang.downcase}&since=monthly").body
   `mkdir -p '#{lang}'`
   File.write("#{lang}/IMGS", html.css(".repo-list-item .avatar").map {|e| e["title"] + " " + e["src"] }.join("\n"))
--- crawler/gravatar.rb
+++ crawler/gravatar.rb
@@ -4,6 +4,7 @@ Dir["*/IMGS"].each do |info|
   dir = File.dirname(info)
   File.read(info).each_line do |line|
     who, url = line.split
-    `wget '#{url}0' -O #{dir}/#{who}.png -q`
+    path = "#{dir}/#{who}.png"
+    `wget '#{url}0' -O #{path} -q` unless File.exists? path
   end
 end
