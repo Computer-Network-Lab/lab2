e42a00edfc1a96f198f263ac62d95087ae145240
Tianshi Li <9413fi@gmail.com>
2015-06-14 00:41:16 +0800

    optimize user experience
    enhancing speed by passing url to index.html
    remove the messes in code description

--- public/js/init.js
+++ public/js/init.js
@@ -15,67 +15,18 @@ $.getUrlParam = function(name) {
     if (r != null) return unescape(r[2]);
     return null;
 }
-var faceid = $.getUrlParam("face");
-var searchid = $.getUrlParam("search");
+var faceurl = $.getUrlParam("faceurl");
+var resulturl = $.getUrlParam("resulturl");
 var code = $.getUrlParam("code");
 var name = $.getUrlParam("name");
 $(function() {
 
-    if (faceid != null) {
-        console.log(faceid);
-        var faceparam = {
-            face_id: faceid,
-            api_key: YOUR_API_KEY,
-            api_secret: YOUR_API_SECRET
-        }
-        $.ajax({
-            url: getFaceUrl + $.param(faceparam),
-            type: 'get',
-            trycount: 0,
-            retryLimit: 3,
-            success: function(data) {
-                console.log(data.face_info[0].url);
-                $('#faceImg').attr("src", data.face_info[0].url);
-            },
-            error: function(xhr, textStatus, errorThrown) {
-                if (textStatus == 'timeout') {
-                    this.trycount++;
-                    if (this.trycount <= this.retryLimit) {
-                        $.ajax(this);
-                        return;
-                    }
-                    return;
-                }
-            }
-        });
+    if (faceurl != null) {
+        $('#faceImg').attr("src", faceurl);
     }
 
-    if (searchid != null) {
-        var searchparam = {
-            face_id: searchid,
-            api_key: YOUR_API_KEY,
-            api_secret: YOUR_API_SECRET
-        }
-        $.ajax({
-            url: getFaceUrl + $.param(searchparam),
-            type: 'get',
-            trycount: 0,
-            retryLimit: 3,
-            success: function(data) {
-                console.log(data.face_info[0].url);
-                $('#searchImg').attr("src", data.face_info[0].url);
-            },
-            error: function(xhr, textStatus, errorThrown) {
-                if (textStatus == 'timeout') {
-                    this.trycount++;
-                    if (this.trycount <= this.retryLimit) {
-                        $.ajax(this);
-                        return;
-                    }
-                    return;
-                }
-            }
-        });
+    if (resulturl != null) {
+        $('#searchImg').attr("src", resulturl);
     }
 
     if (code != null) {
@@ -131,7 +82,6 @@ $(function() {
                         return;
                     }
                     var key_face_id = data.face[0].face_id;
-                    console.log('FACESET' + FACESET);
                     var searchParams = {
                         api_secret: YOUR_API_SECRET,
                         api_key: YOUR_API_KEY,
@@ -149,17 +99,38 @@ $(function() {
                                 var tagarray = data.candidate[0].tag.split("\"");
                                 var tagname = tagarray[1];
                                 var tagcode = tagarray[3];
-                                userImage.set("faceID", faceid);
-                                userImage.save();
-                                progressJs().end();
-
-                                var newParams = {
-                                    face: key_face_id,
-                                    search: faceid,
-                                    code: tagcode,
-                                    name: tagname
+                                var detect_res_param = {
+                                    face_id: faceid,
+                                    api_key: YOUR_API_KEY,
+                                    api_secret: YOUR_API_SECRET
                                 }
-                                window.location.href = "index.html?" + $.param(newParams);
+                                $.ajax({
+                                    url: getFaceUrl + $.param(detect_res_param),
+                                    type: 'get',
+                                    trycount: 0,
+                                    retryLimit: 3,
+                                    success: function(data) {
+                                        progressJs().end();
+
+                                        var newParams = {
+                                            faceurl: detectParams.url,
+                                            resulturl: data.face_info[0].url,
+                                            code: tagcode,
+                                            name: tagname
+                                        }
+                                        window.location.href = "index.html?" + $.param(newParams);
+                                    },
+                                    error: function(xhr, textStatus, errorThrown) {
+                                        if (textStatus == 'timeout') {
+                                            this.trycount++;
+                                            if (this.trycount <= this.retryLimit) {
+                                                $.ajax(this);
+                                                return;
+                                            }
+                                            return;
+                                        }
+                                    }
+                                });
                             } else {
                                 progressJs().end();
                                 setTimeout(function(){Materialize.toast('No face matched', 4000);}, 500);
